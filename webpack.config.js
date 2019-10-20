a5725d3adcd589da3b91b102575628b0b08dfc25
Merlijn Van Holder <merlijn.vanholder@gmail.com>
2019-10-20 21:06:11 +0200

    Added webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  devServer: {
+    contentBase: path.join(__dirname, 'src'),
+    stats: {
+      children: false,
+      maxModules: 0
+    },
+    port: 3001
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Titan',
       template: './src/index.html'
     }),
+    new CopyWebpackPlugin([
+      {
+        from: './src/assets',
+        to: 'assets'
+      }
+    ])
   ],
   output: {
     filename: 'bundle.js',

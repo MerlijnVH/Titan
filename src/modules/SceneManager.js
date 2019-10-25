f1c1159fccead35b8259319f9f361bbaa7e851b3
Merlijn Van Holder <merlijn.vanholder@gmail.com>
2019-10-25 09:59:13 +0200

    Added fog to scene

--- src/modules/SceneManager.js
+++ src/modules/SceneManager.js
@@ -4,6 +4,11 @@ export default class SceneManager {
     constructor() {
         this.scene = new THREE.Scene();
 
+        const color = 0x000000;
+        const near = 2;
+        const far = 8;
+        this.scene.fog = new THREE.Fog(color, near, far);
+
         const axesHelper = new THREE.AxesHelper(5);
         this.scene.add(axesHelper);
     }
